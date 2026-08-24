8e15febd2dbd8d5d756194c768ef4e1934cf39f1
seanrogerson <sean.rogerson@yale.edu>
2026-08-24 10:31:55 -0400

    Add files via upload
    added GUI code

---
+++ GUI.py
@@ -0,0 +1,141 @@
+import tkinter as tk
+from matplotlib.backends.backend_tkagg import FigureCanvasTkAgg
+from matplotlib.figure import Figure
+from tkinter import filedialog, messagebox
+import pandas as pd
+import random
+
+def start_sensors():
+    print("initializing sensors")
+
+def toggle_sensor1():
+    sensor1_line.set_visible(sensor1_checkbox_enable.get() == 1)
+    canvas.draw_idle()
+    
+def toggle_sensor2():
+    sensor2_line.set_visible(sensor2_checkbox_enable.get() == 1)
+    canvas.draw_idle()
+
+def export_data():
+    print("saving data")
+    filename = filedialog.asksaveasfilename(
+        defaultextension=".xlsx",
+        filetypes=[("Excel files", "*.xlsx")]
+    )
+
+    if not filename: #if user cancels save dialog box
+        return
+
+    data = {
+        "Time [Seconds]": x_data,
+        "Sensor 1 [C]": y1,
+        "Sensor 2 [C]": y2
+    }
+
+    df = pd.DataFrame(data)
+    df.to_excel(filename, index=False)
+
+    messagebox.showinfo(
+        "Data Saved",
+        f"Data saved to:\n{filename}"
+    )
+    
+def update_plot(n):
+    dummy_xdata = n
+    dummy_ydata1 = 23*(random.random()/2 + 0.75)
+    dummy_ydata2 = 23*(random.random()/2 + 0.75)
+
+    x_data.append(dummy_xdata)
+    y1.append(dummy_ydata1)
+    y2.append(dummy_ydata2)
+    
+    x_data_plot = x_data[-30:]
+    y1_plot = y1[-30:]
+    y2_plot = y2[-30:]
+
+    sensor1_line.set_data(x_data_plot, y1_plot)
+    sensor2_line.set_data(x_data_plot, y2_plot)
+
+    ax.relim()
+    ax.autoscale_view()
+
+    canvas.draw_idle()
+    window.after(1000, update_plot, n + 1)
+    
+    
+# Initialize the main UI window context
+window = tk.Tk()
+window.title("Temp Reading GUI")
+window.geometry("1000x800")
+
+fig = Figure(figsize=(6, 3), dpi=100)
+ax = fig.add_subplot(111)
+
+# Define your data and plot it
+x_data = []
+y1 = []
+y2 = []
+sensor1_line, = ax.plot(x_data, y1, marker='o', color='b', label='Sensor 1', visible=1)
+sensor2_line, = ax.plot(x_data, y2, marker='o', color='r', label='Sensor 2', visible=0)
+ax.set_title("Sensor Temp")
+ax.set_xlabel("Time [Seconds]")
+ax.set_ylabel("Temp [C]")
+ax.legend(loc="upper right")
+ax.set_ylim(15, 45)
+ax.grid(True)
+
+# 3. Embed the Figure into the Tkinter Canvas
+canvas = FigureCanvasTkAgg(fig, master=window)
+
+# Right-side controls
+control_frame = tk.Frame(window)
+control_frame.pack(side=tk.RIGHT, fill=tk.Y, padx=20)
+
+# Plot
+canvas_widget = canvas.get_tk_widget()
+canvas_widget.pack(side=tk.LEFT, fill=tk.BOTH, expand=True)
+
+# Create a frame for the checkboxes
+control_frame = tk.Frame(window)
+control_frame.pack(side=tk.RIGHT, fill=tk.Y, padx=20)
+
+StartButton = tk.Button(
+    control_frame,
+    text="Start",
+    command=start_sensors
+)
+StartButton.pack(pady=10)
+
+sensor1_checkbox_enable = tk.IntVar(value=1)
+checkbox1 = tk.Checkbutton(
+    control_frame,
+    text="Plot Sensor 1 Data",
+    variable=sensor1_checkbox_enable,
+    command=toggle_sensor1
+)
+checkbox1.pack(pady=10)
+
+sensor2_checkbox_enable = tk.IntVar(value=0)
+checkbox2 = tk.Checkbutton(
+    control_frame,
+    text="Plot Sensor 2 Data",
+    variable=sensor2_checkbox_enable,
+    command=toggle_sensor2
+)
+checkbox2.pack(pady=10)
+
+ExportButton = tk.Button(
+    control_frame,
+    text="Export data to Excel",
+    command=export_data
+)
+ExportButton.pack(pady=10)
+
+
+# 4. Draw the canvas and start the loop
+canvas.draw()
+
+update_plot(0)
+
+# Keeps the window interactive and running indefinitely
+window.mainloop()
